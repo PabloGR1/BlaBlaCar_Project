e6954bebb01d2bdad84e8ee9b7dd2c54905d5571
Pablo Gordo <107139579+PabloGR1@users.noreply.github.com>
2022-10-10 17:24:31 +0200

    Update RegistroBBDD_ST.py

--- RegistroBBDD_ST.py
+++ RegistroBBDD_ST.py
@@ -5,8 +5,40 @@ st.set_page_config(
     page_icon="🚀",
     layout="wide")
 
-st.header('Pablo Gordo')
-st.write('BlaBlaCar Project')
-
-
 st.title('Solicitud de viaje')
+
+nombre = st.text_input('Inserte su nombre: ')
+
+telefono = st.text_input('Inserte su telefono: ', type= "password")
+
+
+# Store the initial value of widgets in session state
+if "visibility" not in st.session_state:
+    st.session_state.visibility = "visible"
+    st.session_state.disabled = False
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.checkbox("Disable text input widget", key="disabled")
+    st.radio(
+        "Set text input label visibility 👉",
+        key="visibility",
+        options=["visible", "hidden", "collapsed"],
+    )
+    st.text_input(
+        "Placeholder for the other text input widget",
+        "This is a placeholder",
+        key="placeholder",
+    )
+
+with col2:
+    text_input = st.text_input(
+        "Enter some text 👇",
+        label_visibility=st.session_state.visibility,
+        disabled=st.session_state.disabled,
+        placeholder=st.session_state.placeholder,
+    )
+
+    if text_input:
+        st.write("You entered: ", text_input)
