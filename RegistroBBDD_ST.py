a34433dde1bb2931258525022b9cc159ff264aed
Pablo Gordo <107139579+PabloGR1@users.noreply.github.com>
2022-10-10 17:27:52 +0200

    Update RegistroBBDD_ST.py

--- RegistroBBDD_ST.py
+++ RegistroBBDD_ST.py
@@ -9,7 +9,11 @@ st.title('Solicitud de viaje')
 
 nombre = st.text_input('Inserte su nombre: ')
 
-telefono = st.text_input('Inserte su telefono: ', type= "password")
+    if nombre:
+        telefono = st.text_input('Inserte su telefono: ', type= "password")
+       
+    else:
+        continue
 
 
 # Store the initial value of widgets in session state
