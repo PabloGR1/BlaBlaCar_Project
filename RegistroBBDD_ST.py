0688cedf85c7a9828bb9695cf6a1f497fd73bc28
Pablo Gordo <107139579+PabloGR1@users.noreply.github.com>
2022-10-11 10:46:24 +0200

    Update RegistroBBDD_ST.py

--- RegistroBBDD_ST.py
+++ RegistroBBDD_ST.py
@@ -6,11 +6,4 @@ db=cursor.BlaBlaCar # bbdd
 colec_solicitud=db.Solicitudes # tabla solicitudes
 colec_api=db.viajes_api_v3
 
-st.text_input('prueba')
-
-import os
-
-os.system("pip3 install pymongo")
-import pymongo
-
-st.text("This is my first Mongo")
+nombre = st.text_input('Inserte su nombre: ')
