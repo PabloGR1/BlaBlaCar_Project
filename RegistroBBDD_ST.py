c2a8cc88a89f7676b1e07ed453df812aa858f968
Pablo Gordo <107139579+PabloGR1@users.noreply.github.com>
2022-10-11 10:47:17 +0200

    Update RegistroBBDD_ST.py

--- RegistroBBDD_ST.py
+++ RegistroBBDD_ST.py
@@ -7,3 +7,6 @@ colec_solicitud=db.Solicitudes # tabla solicitudes
 colec_api=db.viajes_api_v3
 
 nombre = st.text_input('Inserte su nombre: ')
+
+if nombre:
+  origen = st.text_input('Inserte la provincia de origen: ')
