12b392f588d5ad7fc0a6c701f688f6467c6f8eca
Pablo Gordo <107139579+PabloGR1@users.noreply.github.com>
2022-10-11 10:52:20 +0200

    Update RegistroBBDD_ST.py

--- RegistroBBDD_ST.py
+++ RegistroBBDD_ST.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from datetime import datetime
 import pymongo
 from pymongo import MongoClient
 cursor=MongoClient('mongodb://localhost:27017')
@@ -10,3 +11,23 @@ nombre = st.text_input('Inserte su nombre: ')
 
 if nombre:
   origen = st.text_input('Inserte la provincia de origen: ')
+
+id = datetime.today().strftime('%Y%m%d%H%M%S')
+origen = origen.capitalize()
+destino = destino.capitalize()
+precio = str(precio)
+email = email.lower()
+nombre = nombre.title()
+telefono = str('+34') + str(telefono)
+
+if st.button('Registrar'):
+  columns = ['ID', 'NOMBRE', 'ORIGEN']
+  datos = [id, nombre, origen]
+  data = dict(zip(columns, datos))
+  colec_solicitud.insert_one(data);
+  st.write('Registrado')
+  
+else:
+    st.write('No Registrado')
+
+
