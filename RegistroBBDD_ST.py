27ec789176832f91adcf6f18708220ae0200dab5
Pablo Gordo <107139579+PabloGR1@users.noreply.github.com>
2022-10-12 12:40:59 +0200

    Update RegistroBBDD_ST.py

--- RegistroBBDD_ST.py
+++ RegistroBBDD_ST.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import pymongo
 
 from pymongo import MongoClient
-client = pymongo.MongoClient("mongodb+srv://PGR:PGR123@cluster0.oxuoatg.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
+client = pymongo.MongoClient("mongodb+srv://PGR:PGR123@cluster0.oxuoatg.mongodb.net/?retryWrites=true&w=majority")
 db = client.test
 colec_solicitud=db.Solicitudes # tabla solicitudes
 colec_api=db.viajes_api_v3
