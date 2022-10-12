cc4a56c9cc135f155e4ed0e9871bbfaf07afce0c
Pablo Gordo <107139579+PabloGR1@users.noreply.github.com>
2022-10-12 12:08:45 +0200

    Update RegistroBBDD_ST.py

--- RegistroBBDD_ST.py
+++ RegistroBBDD_ST.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import pymongo
 
 from pymongo import MongoClient
-cursor=MongoClient('mongodb://localhost:27017')
+cursor=MongoClient('mongodb+srv://PGR:PGR123@cluster0.oxuoatg.mongodb.net/?retryWrites=true&w=majority'')
 db=cursor.BlaBlaCar # bbdd
 colec_solicitud=db.Solicitudes # tabla solicitudes
 colec_api=db.viajes_api_v3
