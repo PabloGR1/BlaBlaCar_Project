5fd6e7354d31eb2fc80660dc1c7863111fc30e1a
Pablo Gordo <107139579+PabloGR1@users.noreply.github.com>
2022-10-12 12:06:59 +0200

    Update RegistroBBDD_ST.py

--- RegistroBBDD_ST.py
+++ RegistroBBDD_ST.py
@@ -3,10 +3,12 @@ from datetime import datetime
 import pymongo
 
 from pymongo import MongoClient
-cursor=MongoClient('mongodb://localhost:27017')
-db=cursor.BlaBlaCar # bbdd
-colec_solicitud=db.Solicitudes # tabla solicitudes
-colec_api=db.viajes_api_v3
+cursornube=MongoClient(f'mongodb+srv://PGR:PGR123@cluster0.oxuoatg.mongodb.net/?retryWrites=true&w=majority')
+dbnube=cursornube.BlaBlaCar # bbdd
+colec_api_nube=dbnube.viajes_api_v3 # tabla filtrada api
+colec_bruto_nube=dbnube.dicc_bruto # tabla bruto api
+colec_solicitud_nube=dbnube.Solicitudes # tabla solicitudes
+
 
 nombre = st.text_input('Inserte su nombre: ')
 nombre = nombre.title()
