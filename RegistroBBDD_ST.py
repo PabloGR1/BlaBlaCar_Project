1fe22dccce9b9d86fd25d6155faec51c66b37be1
Pablo Gordo <107139579+PabloGR1@users.noreply.github.com>
2022-10-12 12:48:19 +0200

    Update RegistroBBDD_ST.py

--- RegistroBBDD_ST.py
+++ RegistroBBDD_ST.py
@@ -2,6 +2,7 @@ import streamlit as st
 from datetime import datetime
 import pymongo
 
+@st.cache(allow_output_mutation=True)
 client = pymongo.MongoClient("mongodb+srv://JF:JF@cluster0.oxuoatg.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
 db = client.test
 colec_solicitud=db.Solicitudes # tabla solicitudes
